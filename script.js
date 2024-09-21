2c9a55647bea430021606849133abedd025b4d98
kozochkina82 <kozochkina82@yandex.ru>
2024-09-22 00:44:35 +0500

    fix: make audio playing right

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* 1 экран */
 const firstRight = document.querySelector('.first-right');
 firstRight.addEventListener('click', changeScreenOneRight);
 function changeScreenOneRight(){
@@ -11,40 +12,43 @@ function changeScreenOneLeft(){
     document.querySelector('.screen-1').classList.add('invisible');
     document.querySelector('.screen-3').classList.remove('invisible');
 }
-document.querySelector('.second-right').onclick = function() {changeScreenTwoRight()};
+/* 2 экран */
+const secondRight = document.querySelector('.second-right');
+secondRight.addEventListener('click', changeScreenTwoRight);
 function changeScreenTwoRight(){
     document.querySelector('.screen-2').classList.add('invisible');
     document.querySelector('.screen-3').classList.remove('invisible');
 }
-document.querySelector('.second-left').onclick = function() {changeScreenTwoLeft()};
+
+const secondLeft = document.querySelector('.second-left');
+secondLeft.addEventListener('click', changeScreenTwoLeft);
 function changeScreenTwoLeft(){
     document.querySelector('.screen-2').classList.add('invisible');
     document.querySelector('.screen-1').classList.remove('invisible');
 }
-document.querySelector('.third-right').onclick = function() {changeScreenThreeRight()};
+
+/* 3 экран */
+const thirdRight = document.querySelector('.third-right');
+thirdRight.addEventListener('click', changeScreenThreeRight);
 function changeScreenThreeRight(){
     document.querySelector('.screen-3').classList.add('invisible');
     document.querySelector('.screen-1').classList.remove('invisible');
 }
-document.querySelector('.third-left').onclick = function() {changeScreenThreeLeft()};
+
+const thirdLeft = document.querySelector('.third-left');
+thirdLeft.addEventListener('click', changeScreenThreeLeft);
 function changeScreenThreeLeft(){
     document.querySelector('.screen-3').classList.add('invisible');
     document.querySelector('.screen-2').classList.remove('invisible');
 }
-
-//const audio1 = document.getElementById('song-1');
-//document.querySelect('.play-first').onclick = function(){mySong1()};
-//function mySong1(){
-//    audio1.play();
-//}
-
+/*Song 1*/
 const audio1 = document.querySelector('.audio1');
 const playBtn1 = document.querySelector('.play-first');
 const pauseBtn1 = document.querySelector('.pause-first');
 function playAudio1() {
+    audio1.currentTime = 0;
     audio1.play();
   }
-  
   function pauseAudio1() {
     audio1.pause();
   }
@@ -53,25 +57,36 @@ function playAudio1() {
   pauseBtn1.addEventListener('click', pauseAudio1);
   firstRight.addEventListener('click', pauseAudio1);
   firstLeft.addEventListener('click', pauseAudio1);
+  firstRight.addEventListener('click', playAudio2);
+  firstLeft.addEventListener('click', playAudio3);
 
+/*Song 2 */
 const audio2 = document.querySelector('.audio2');
 const playBtn2 = document.querySelector('.play-second');
 const pauseBtn2 = document.querySelector('.pause-second');
 function playAudio2() {
+    audio2.currentTime = 0;
     audio2.play();
   }
-  
+
   function pauseAudio2() {
     audio2.pause();
-  }
-  
+}
   playBtn2.addEventListener('click', playAudio2);
   pauseBtn2.addEventListener('click', pauseAudio2);
+  secondRight.addEventListener('click', pauseAudio2);
+  secondLeft.addEventListener('click', pauseAudio2);
+  secondRight.addEventListener('click', playAudio3);
+  secondLeft.addEventListener('click', playAudio1);
+  
+
+  /* Song 3 */
 
 const audio3 = document.querySelector('.audio3');
 const playBtn3 = document.querySelector('.play-third');
 const pauseBtn3 = document.querySelector('.pause-third');
 function playAudio3() {
+    audio3.currentTime = 0;
     audio3.play();
   }
   
@@ -81,4 +96,8 @@ function playAudio3() {
   
   playBtn3.addEventListener('click', playAudio3);
   pauseBtn3.addEventListener('click', pauseAudio3);
+thirdRight.addEventListener('click', pauseAudio3,);
+thirdLeft.addEventListener('click', pauseAudio3);
+thirdRight.addEventListener('click', playAudio1,);
+thirdLeft.addEventListener('click', playAudio2);
   
